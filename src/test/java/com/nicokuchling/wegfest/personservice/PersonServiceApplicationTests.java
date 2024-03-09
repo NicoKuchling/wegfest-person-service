0f8f69a4b178a75b9810b938f55d08c9e7075a32
Nico Kuchling <nico.kuchling@outlook.de>
2024-03-09 14:33:52 +0100

    Person-service initial commit

---
+++ src/test/java/com/nicokuchling/wegfest/personservice/PersonServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicokuchling.wegfest.personservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
